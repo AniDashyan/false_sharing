fc5833b7f3231fd7b8ba0c89bf5d1a50125596d1
AniDashyan <ani.dashyan77@gmail.com>
2025-04-16 17:14:58 +0400

    Refactor main.cpp to implement performance comparison of non-threaded and threaded counter increments
    - Introduce two structures: CountersAdjacent and CountersPadded to test false sharing and padding effects.
    - Add functions for non-threaded and threaded counter increments.
    - Implement command-line argument parsing for iterations and runs.
    - Add timing and logging for each run to compare performance across different methods.
    - Add test cases to CMake

--- main.cpp
+++ main.cpp
@@ -1,39 +1,97 @@
 #include <iostream>
-#include <chrono>
 #include <thread>
+#include <utility>
+#include "kaizen.h"
 
-using namespace std::chrono;
-using ms = std::chrono::milliseconds;
-
-struct A {
+struct CountersAdjacent {
     int x = 0;
     int y = 1;
-} a;
+};
 
-void fx() {
-    for (int i = 0; i < 1'000'000'000; i++)
-        a.x++;
-}
+struct CountersPadded {
+   int x = 0;
+   char padding[64 - sizeof(int)];
+   int y = 0;
+};
 
-void fy() {
-    int sum = 0;
-    for (int i = 0; i < 1'000'000'000; i++)
-        sum += a.y;
-}
-
-int main() {
-    for (int i = 0; i < 20; i++) {
-        auto t1 = steady_clock::now();
-        // fx();
-        // fy();
-
-        std::thread tx(fx);
-        std::thread ty(fy);
-
-        tx.join();
-        ty.join();
-        auto t2 = steady_clock::now();
-
-        std::cout << "Time: " << duration_cast<ms>(t2 - t1).count() << " ms\n";
+void increment_counter(int* counter, int iterations) {
+    for (int i = 0; i < iterations; ++i) {
+        (*counter)++;
     }
+}
+
+void run_non_threaded(CountersAdjacent& counters,int iterations, zen::timer& t) {
+    t.start();
+    increment_counter(&counters.x, iterations);
+    increment_counter(&counters.y, iterations);
+    t.stop();
+    zen::log("Non-threaded time: ", t.duration<zen::timer::msec>().count()," ms");
+}
+
+void run_threaded_adjacent(CountersAdjacent& counters,int iterations, zen::timer& t) {
+    t.start();
+    std::thread t1(increment_counter, &counters.x, iterations);
+    std::thread t2(increment_counter, &counters.y, iterations);
+    t1.join();
+    t2.join();
+    t.stop();
+    zen::log("Threaded (false sharing) time: " ,t.duration<zen::timer::msec>().count() ," ms");
+}
+
+void run_threaded_padded(CountersPadded& counters,int iterations, zen::timer& t) {
+    t.start();
+    std::thread t1(increment_counter, &counters.x, iterations);
+    std::thread t2(increment_counter, &counters.y, iterations);
+    t1.join();
+    t2.join();
+    t.stop();
+    zen::log("Threaded (padded) time: " ,t.duration<zen::timer::msec>().count() ," ms");
+}
+
+std::pair<int, int> parse_args(int argc, char** argv) {
+    zen::cmd_args args(argv, argc);
+    int iter = 100'000'000;
+    int runs = 5;
+    if (!args.is_present("--iter")|| !args.is_present("--runs")) {
+        zen::log(zen::color::yellow("No --iter or --runs provided. Using default values: "));
+        return {iter, runs};
+    } else {
+        iter = std::stoi(args.get_options("--iter")[0]);
+        runs = std::stoi(args.get_options("--runs")[0]);
+    }
+    return {iter, runs};
+}
+
+int main(int argc, char** argv) {
+
+    auto [iterations, runs] = parse_args(argc, argv);
+    CountersAdjacent counters_adjacent;
+    CountersPadded counters_padded;
+    zen::timer t;
+
+    auto total_non_threaded = 0, total_adjacent = 0, total_padded = 0;
+
+    for (int i = 0; i < runs; ++i) {
+        zen::log("\nRun " ,i + 1 ,":");
+
+       
+        run_non_threaded(counters_adjacent, iterations, t);
+        total_non_threaded += t.duration<zen::timer::msec>().count();
+        
+    
+        run_threaded_adjacent(counters_adjacent, iterations, t);
+        total_adjacent += t.duration<zen::timer::msec>().count();
+     
+
+        run_threaded_padded(counters_padded, iterations, t);
+        total_padded += t.duration<zen::timer::msec>().count();
+      
+    }
+
+    zen::log("\nAverage times over " ,runs ," runs:");
+    zen::log("Non-threaded: " ,total_non_threaded / runs ," ms");
+    zen::log("Threaded (false sharing): " ,total_adjacent / runs ," ms");
+    zen::log("Threaded (padded): " ,total_padded / runs ," ms");
+
+    return 0;
 }
